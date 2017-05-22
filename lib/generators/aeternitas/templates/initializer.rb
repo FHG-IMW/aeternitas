1bf1b0b7dd85e4392e01e68553018d1cc7123109
Max <max@kopfueber.org>
2017-05-22 16:33:24 +0200

    [#4] Fix Redis default config
    - use host parameter instead of url

--- lib/generators/aeternitas/templates/initializer.rb
+++ lib/generators/aeternitas/templates/initializer.rb
@@ -1,6 +1,6 @@
 # Configure Aeternitas
 Aeternitas.configure do |config|
-  config.redis = { url: 'localhost', port: 6379 } #this is the default Redis config which should work in most cases.
+  config.redis = { host: 'localhost', port: 6379 } #this is the default Redis config which should work in most cases.
 end
 
 Sidekiq.configure_server do |config|
